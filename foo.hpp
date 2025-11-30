1e180c4db14c5983104291fe9a20a90d3a498b37
wikipoz000-netizen <wikipoz000@gmail.com>
2025-11-30 22:24:44 +0100

    Add implementation for birthday and monster check
    Implement birthday logic and monster check in foo function.

--- foo.hpp
+++ foo.hpp
@@ -10,7 +10,11 @@ std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
-    // Twoja implementacja tutaj
+    std::for_each(humans.begin(), humans.end(),
+                  [&result, i = humans.size()](Human& h) mutable {
+                      h.birthday();
+                      result[--i] = h.isMonster() ? 'n' : 'y';
+                  });
 
     return retval;
 }
